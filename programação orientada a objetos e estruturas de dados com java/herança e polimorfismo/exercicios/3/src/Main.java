499dbc8a6a625580ccc3f0129ffac606929f007b
Leonardo Naime <leonardo.naime@exati.com.br>
2025-07-07 21:07:49 -0300

    Herança e Polimorfismo

---
+++ programação orientada a objetos e estruturas de dados com java/herança e polimorfismo/exercicios/3/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Clock brlclock = new BRLClock();
+        brlclock.setSecond(0);
+        brlclock.setMinute(0);
+        brlclock.setHour(15);
+
+        System.out.println(brlclock.getTime());
+
+        System.out.println(new USClock().convert(brlclock).getTime());
+    }
+}
